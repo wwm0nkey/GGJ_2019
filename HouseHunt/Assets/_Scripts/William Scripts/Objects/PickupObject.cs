9b493e385902cd84acef851212af1cb87d17632a
William Burke <wwm0nkey@Outlook.com>
2019-01-26 11:14:50 -0500

    Added FPS controller, Gravity Gun

---
+++ HouseHunt/Assets/_Scripts/William Scripts/Objects/PickupObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    public GameObject pickuplocation;
+    public Rigidbody rb;
+    public bool isPicked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        pickuplocation = GameObject.FindGameObjectWithTag("PickUpLocation");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPicked == true)
+        {
+            this.gameObject.transform.position = pickuplocation.transform.position;
+        }
+    }
+
+    public void isPickedUp()
+    {
+        rb.useGravity = false;
+
+        //isPicked = true;
+        //this.gameObject.transform.position = pickuplocation.transform.position;
+
+    }
+    public IEnumerator pick()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+    }
+
+}
---
+++ HouseHunt/Assets/_Scripts/William Scripts/Weapons/GravityGun.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityGun : MonoBehaviour
+{
+
+    public float pickupRange = 100f;
+    public Camera playerCam;
+    public GameObject itemLocation;
+    public GameObject item;
+    public bool hasItem = false;
+    public LayerMask itemMask;
+    public GameObject prevItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCam = this.GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && hasItem == false)
+        {
+            Vector3 rayOrigin = playerCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.5f));
+
+            RaycastHit hit;
+            Ray ray = playerCam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, pickupRange, itemMask))
+            {
+
+                Debug.Log(hit.collider.name);
+                //                    hit.collider.GetComponent<PickupObject>().isPickedUp();
+                if (hit.collider.GetComponent<Rigidbody>() != null)
+                {
+                    item = hit.collider.gameObject;
+                    item.transform.SetParent(this.gameObject.transform);
+                    item.GetComponent<Rigidbody>().isKinematic = true;
+                    item.transform.position = itemLocation.transform.position;
+                    hasItem = true;
+
+                }
+            }
+        }
+        else if (Input.GetButtonDown("Fire1") && hasItem == true)
+        {
+            prevItem = item;
+            item.transform.parent = null;
+            item.GetComponent<Rigidbody>().isKinematic = false;
+            hasItem = false;
+            prevItem.GetComponent<Rigidbody>().AddForce(transform.forward * 500);
+        }
+    }
+}
